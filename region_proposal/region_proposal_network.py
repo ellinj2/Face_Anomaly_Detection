f28274fb804f657aaa17038d5427cec189971d5f
Ohad Nir <45518501+OhadNir@users.noreply.github.com>
2022-10-31 19:00:03 -0400

    Region proposal network functions.

--- region_proposal/region_proposal_network.py
+++ region_proposal/region_proposal_network.py
@@ -1,5 +1,11 @@
+import os
+import torch
 from torch import nn
 from functools import partial
+
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import numpy as np
 
 from torchvision.ops import misc, feature_pyramid_network
 from torchvision.models.resnet import ResNet50_Weights, resnet50
@@ -7,16 +13,42 @@ from torchvision.models.resnet import ResNet50_Weights, resnet50
 from torchvision.models.detection import backbone_utils, RetinaNet, FCOS
 from torchvision.models.detection.retinanet import RetinaNetHead, _default_anchorgen
 
-
 class RegionProposalNetowrk():
     """
     Class to interact with the region proposal network.
     """
 
-    def __init__(self, load_path=None):
-        pass
+    def __init__(self, 
+                model_type, 
+                load_path=None, 
+                trainable_backbone_layers=None, 
+                pretrained_backbone=True, 
+                progress=False, 
+                **kwargs):
 
-    def __retinanet(self, trainable_backbone_layers, pretrained_backbone=True, progress=False, **kwargs):
+        if pretrained_backbone and trainable_backbone_layers is None:
+            trainable_backbone_layers = 3
+
+        if model_type == "retinanet":
+            self._model = self.__retinanet(trainable_backbone_layers, 
+                                            pretrained_backbone=True, 
+                                            progress=progress, 
+                                            **kwargs)
+        elif model_type == "fcos":
+            self._model = self.__fcos(trainable_backbone_layers, 
+                                        pretrained_backbone=True, 
+                                        progress=progress, 
+                                        **kwargs)
+        else:
+            raise ValueError(f"Model type '{model_type}' is not an available model type.")
+
+        self.to(torch.device('cpu')) # defaults to CPU.
+        
+        if load_path:
+            self.load(load_path)
+
+
+    def __retinanet(self, trainable_backbone_layers=3, pretrained_backbone=True, progress=False, **kwargs):
         """
         Builds RetinaNet model.
         Code inspired by:
@@ -71,31 +103,122 @@ class RegionProposalNetowrk():
         """
         Returns the model parameters.
         """
-        pass
+        return self._model.parameters()
 
     def to(self, device):
         """
         Loads and performs computations on the model and input data to specified device.
         """
-        pass
+        try:
+            self._model.to(device)
+            self.device = device
+        except Exception as e:
+            raise Exception(e)
+         
 
     def save(self, save_path, save_name):
         """
         Save the model to specified path.
         """
-        pass
+        save_file = os.path.join(save_path, save_name)
+        torch.save(self._model.state_dict(), save_file)
+
+        return save_file
 
     def load(self, load_path):
         """
         Load the model from spesified path.
         """
-        pass
+        self._model.load_state_dict(torch.load(load_path, map_location=self.device))
 
     def fit(self, epochs, datasets, batch_size, optimizer, save_path, checkpoints=0, progress=False):
         """
         Fits the model to the training dataset and evaluates on the validation dataset
         """
-        pass
+        if not os.path.isdir(save_path):
+            os.makedirs(save_path)
+
+        train_dataset, valid_dataset = datasets
+
+        train_loader = DataLoader(train_dataset, 
+                                    batch_size=batch_size, 
+                                    shuffle=True, 
+                                    num_workers=0, ### try to change these settings if dataloading is slow.
+                                    collate_fn=__dataset_formatting,
+                                    pin_memory=False,
+                                    persistent_workers=False)
+
+        if isinstance(optimizer, tuple):
+            sched, optim = optimizer
+        else:
+            sched, optim = (None, optimizer)
+
+        train_hist = {
+            "train_loss": [],
+            "train_map": [],
+            "train_map_50": [],
+            "train_map_75": [],
+            "valid_loss": [],
+            "valid_map": [],
+            "valid_map_50": [],
+            "valid_map_75": []
+        }
+
+        for e in range(epochs):
+            self._model.train()
+
+            if progress:
+                print(f"Epoch: {e+1}")
+
+                train_e_loader = tqdm(train_loader)
+                train_e_loader.set_description(desc=f"Training loss: {np.nan}")
+            else:
+                train_e_loader = train_loader
+            
+            loss = 0
+            for i, (X, y) in enumerate(train_e_loader):
+                X = [x.to(self.device) for x in X]
+                y = [{k: v.to(self.device) for k, v in t.items()} for t in y]
+
+                loss_dict = self.model(X, y)
+
+                batch_loss = sum(b_loss for b_loss in loss_dict.values())
+                loss += batch_loss.item()
+
+                optim.zero_grad()
+                batch_loss.backward()
+                optimizer.step()
+
+                if progress:
+                    train_e_loader.set_description(desc=f"Training loss: {loss/(i+1):.4f}")
+
+            train_metrics = self.evaluate(train_dataset, batch_size, progress=progress)
+            valid_metrics = self.evaluate(valid_dataset, batch_size, progress=progress)
+            
+            ### add loss
+            train_hist["train_map"].append(train_metrics["map"])
+            train_hist["train_map_50"].append(train_metrics["map_50"])
+            train_hist["train_map_75"].append(train_metrics["map_75"])
+            
+            ### add loss
+            train_hist["valid_map"].append(valid_metrics["map"])
+            train_hist["valid_map_50"].append(valid_metrics["map_50"])
+            train_hist["valid_map_75"].append(valid_metrics["map_75"])
+
+            if progress:
+                ### print training progress.
+                pass
+
+            if checkpoints > 0:
+                if e % checkpoints == 0:
+                    self.save(save_path, f"checkpoint_{e+1}.pth")
+        
+        file_path = self.save(save_path, "final_model.pth")
+
+        if progress:
+            print(f"Saved final model to '{file_path}'.")
+
+        return train_hist
 
     def propose(self, X):
         """
@@ -108,3 +231,17 @@ class RegionProposalNetowrk():
         Evaluates the model on a dataset.
         """
         pass
+
+def __dataset_formatting(data):
+    """
+    Formats data from dataset object to model compatiable data structure.
+    """
+    images, targets = [], []
+    
+    for d in data:
+        images.append(d[0])
+        targets.append(d[1])
+        
+    return images, targets
+
+
