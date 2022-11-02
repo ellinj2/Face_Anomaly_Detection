d115cdc74b4545430b47ac440c06039930fe67b8
Jesse Ellin <jesse.ellin@gmail.com>
2022-11-02 00:14:51 -0400

    Implemented __call__

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,2 +1,9 @@
 class Pipeline:
-	pass
+	def __init__(self, models=[], **kwargs):
+		self.models = models
+		self.args = kwargs
+
+	def __call__(self, X, **kwargs):
+		for model in self.models:
+			X = model(X, **kwargs)
+		return X
