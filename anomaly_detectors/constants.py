8f003f6fb9fccb3f9d12b4cc68266a3917b3b3c2
Jesse Ellin <jesse.ellin@gmail.com>
2022-11-02 01:21:32 -0400

    Added OneClassSVM, IsolationForest, and LocalOutlierFactor models

--- anomaly_detectors/constants.py
+++ anomaly_detectors/constants.py
@@ -1,7 +1,14 @@
+import numpy as np
+from sklearn.svm import OneClassSVM
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
 LOSS_MAP = {
 	"contrastive flow" : lambda x,y : x - y,
 	"likelihood ratio" : lambda x,y : x / y,
+	"log-likelihood ratio" : lambda x,y : np.log(x/y)
 }
 DETECTOR_MAP = {
-	
+	"one-class SVM" : OneClassSVM,
+	"isolation forest" : IsolationForest,
+	"local outlier factor" : LocalOutlierFactor
 }
